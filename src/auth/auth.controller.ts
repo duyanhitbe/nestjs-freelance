9e8fd355d8de3b5dc58730ff5e063f6d417eef06
duyanhitbe <duyanh.it.work@gmail.com>
2025-01-02 14:47:51 +0700

    feat: auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,19 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { SwaggerOkResponse } from '@lib/common/decorators';
+import { Body, Controller, Get, HttpCode, Post, Req, Res } from '@nestjs/common';
+import { SwaggerOkResponse, UseAuth, User } from '@lib/common/decorators';
 import { LoginUserDto, LoginUserEntity } from '@lib/modules/auth';
 import { LoginUserUseCase } from './usecases/login-user.usecase';
+import { Request, Response } from 'express';
+import { ENUM_TOKEN_ROLE } from '@lib/core/jwt';
+import { RequestUser } from '@lib/common/interfaces';
+import { UserEntity } from '@lib/modules/user';
+import { DetailUserUseCase } from '../user/usecases/detail-user.usecase';
 
 @Controller('auth')
 export class AuthController {
-	constructor(private readonly loginUserUseCase: LoginUserUseCase) {}
+	constructor(
+		private readonly loginUserUseCase: LoginUserUseCase,
+		private readonly detailUserUseCase: DetailUserUseCase
+	) {}
 
 	/**
 	 * @path POST /api/v1/auth/user/login
@@ -17,5 +25,30 @@ export class AuthController {
 	@SwaggerOkResponse({ summary: 'Login user', type: LoginUserEntity })
 	loginUser(@Body() data: LoginUserDto): Promise<LoginUserEntity> {
 		return this.loginUserUseCase.execute(data);
+	}
+
+	/**
+	 * @path POST /api/v1/auth/logout
+	 * @returns Promise<void>
+	 */
+	@UseAuth()
+	@Post('logout')
+	@HttpCode(200)
+	@SwaggerOkResponse({ summary: 'Logout', type: String })
+	async logout(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+		req.session.destroy(() => {});
+		res.clearCookie('session');
+		return 'success';
+	}
+
+	/**
+	 * @path GET /api/v1/auth/user
+	 * @returns Promise<UserEntity>
+	 */
+	@UseAuth({ roles: [ENUM_TOKEN_ROLE.USER] })
+	@Get('user')
+	@SwaggerOkResponse({ summary: 'Get user info', type: UserEntity })
+	async getUserInfo(@User() user: RequestUser) {
+		return this.detailUserUseCase.query(user!.id);
 	}
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { LoginUserUseCase } from './usecases/login-user.usecase';
+import { UserModule } from '../user/user.module';
 
 @Module({
+	imports: [UserModule],
 	controllers: [AuthController],
 	providers: [LoginUserUseCase]
 })
